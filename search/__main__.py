760cb54137e4220ff5bb0485beed6b13ff0ed084
Cj <chanjieh@student.unimelb.edu.au>
2020-04-06 00:40:43 +1000

    Changed the move and boom function to alter and pass on a deep copy of the state rather than altering the original state as well as adding functions to print out the output that's needed

--- search/__main__.py
+++ search/__main__.py
@@ -62,6 +62,7 @@ def store(data):
 def move(state, n, x, y, dir, no_steps):
 
     # Check if move will still be on the board
+    new_state = state.copy()
     new_x = x
     new_y = y
 
@@ -81,28 +82,28 @@ def move(state, n, x, y, dir, no_steps):
         return None
 
     # Remove the n pieces from the original tile 
-    old_n = int(state[(x,y)][0])
+    old_n = int(new_state[(x,y)][0])
 
-    # If moving less than a full stack
+    # If moving part of a stack
     if old_n > n:
         old_n -= n
-        state[(x,y)] = "{} W".format(old_n)
+        new_state[(x,y)] = "{} W".format(old_n)
 
-    # If moving full stack
+    # If moving whole stack
     else:
-        del state[(x,y)]
+        del new_state[(x,y)]
 
     # Place n pieces at destination 
     new_n = n
 
     # If there are already pieces on the destination tile
-    if (new_x, new_y) in state:
+    if (new_x, new_y) in new_state:
 
         # If piece at destination is white then just make/add to stack
-        c = state[(new_x, new_y)][2]
+        c = new_state[(new_x, new_y)][2]
 
         if c == "W":
-            new_n += int(state[(new_x, new_y)][0])
+            new_n += int(new_state[(new_x, new_y)][0])
 
         # If piece at destination is black then is an invalid move
         else:
@@ -110,24 +111,27 @@ def move(state, n, x, y, dir, no_steps):
 
     # Store and return the new state with the moved tiles
     state[(new_x, new_y)] = "{} W".format(new_n)
-    return state
+    return new_state
 
 # Boom function
 # Deletes the exploded pieces from board_dict
 # takes a state and the location of the blast and returns the updated state
 def boom(state, x, y):
+
+    new_state = state.copy()
     
     # Remove all pieces at location
-    if (x,y) in state:
-        del state[(x,y)]
+    if (x,y) in new_state:
+        del new_state[(x,y)]
 
     # Check for surrounding tiles if any other pieces caught in explosion
     for surrounding_x in range(x-1, x+2):
         for surrounding_y in range(y-1,y+2):
-            if (surrounding_x, surrounding_y) in state:
-                state = boom(state, surrounding_x,surrounding_y)
+            if (surrounding_x, surrounding_y) in new_state:
+                new_state = boom(new_state, surrounding_x,surrounding_y)
     
-    return state 
+    return new_state 
+
 
 # search the optimal tiles/tile where the white pieces/piece should move to
 # modelling the problem - States - white and black pieces along with their location
@@ -142,9 +146,11 @@ def boom(state, x, y):
 # state is a dictionary of white/black as keys and list of coordinates as values
 def heuristic(state):
     total_dis = 0
+    white_freq = 0
     for nxy in state["white"]:
         x = nxy[1]
         y = nxy[2]
+        white_freq = len(nxy)
         for mpq in state["black"]:
             p = mpq[1]
             q = mpq[2]
@@ -153,44 +159,65 @@ def heuristic(state):
     return total_dis
 
 
+# class holds all the variables needed to define a unique move
+class Move():
 
+    def __init__(self, n, x_a, y_a, x_b, y_b):
+        self.n = n
+        self.x_a = x_a
+        self.x_b = x_b
+        self.y_a = y_a
+        self.y_b = y_b
+
+# class holds all the variables needed to define a unique boom
+class Boom():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+# a node class that holds the current state, parent state, f, g, h values
+# it also stores the action which tells us how the state was achieved from the last one
 class Node():
     """A node class for A* Pathfinding"""
 
-    def __init__(self, parent=None, state=None):
+    def __init__(self, parent=None, state=None, action=None):
         self.parent = parent
         self.state = state
+        self.action = action
 
         self.g = 0
         self.h = 0
         self.f = 0
 
+    # equality is defined if the current state is the same for any 2 nodes
     def __eq__(self, other):
         return self.state == other.state
 
 
-
-
 def a_star_search(state):
-
     # initialise the starting node
-    start_node = Node(None, state)
+    start_node = Node(None, state, None)
     start_node.g = 0
     start_node.h = heuristic(state)
     start_node.f = start_node.h
 
     # Initialize both open and closed list
+    # open list has all the unvisited nodes and the closed list has the visited ones
     open_list = []
     closed_list = []
 
     # Add the start node
     open_list.append(start_node)
 
+    # loop till the end
     while len(open_list) > 0:
         # Get the current node
         current_node = open_list[0]
         current_index = 0
 
+        # check if some other node might be optimal to traverse based on the evaluation function
         for index, item in enumerate(open_list):
             if item.f < current_node.f:
                 current_node = item
@@ -200,20 +227,20 @@ def a_star_search(state):
         open_list.pop(current_index)
         closed_list.append(current_node)
 
-        # if the current node has no black piece it is a goal node hence return
+        # if the current node has no black piece, it is a goal node hence return
         for nxy in current_node.state["black"]:
             if len(nxy) == 0:
                 path = []
                 current = current_node
                 while current is not None:
-                    path.append(current.state)
+                    path.append(current.action)
                     current = current.parent
-                return path[::-1]  # Return reversed path
+                return path[::-1]  # Return reversed path of actions either move or boom
 
             else:
                 break
 
-        # Generate children
+        # Generate children, if goal not reached and further traversal is needed
         children = []
 
         # Generating children by moving
@@ -222,22 +249,24 @@ def a_star_search(state):
             n = nxy[0]
             x = nxy[1]
             y = nxy[2]
-            for i in list(range(1, n+1)):
-                for pieces in list(range(1,n+1)):
+            for i in list(range(1, n + 1)):
+                for pieces in list(range(1, n + 1)):
                     for s in dirs:
-                        temp = move(current_node.state, pieces, x, y, s, i)
-                        new_node = Node(current_node, temp)
+                        (temp_state, new_x, new_y) = move(current_node.state, pieces, x, y, s, i)
+                        if temp_state is None:
+                            continue
+                        temp_act = Move(pieces, x, y, new_x, new_y)
+                        new_node = Node(current_node, temp_state, temp_act)
                         children.append(new_node)
-
 
         # Generating children by blasting
         for nxy in current_node.state["white"]:
             x = nxy[1]
             y = nxy[2]
             temp = boom(current_node.state, x, y)
-            new_node = Node(current_node, temp)
+            temp_act = Boom(x, y)
+            new_node = Node(current_node, temp, temp_act)
             children.append(new_node)
-
 
         # Loop through children
         for child in children:
@@ -265,6 +294,7 @@ def a_star_search(state):
                 else:
                     # Add the child to the open list
                     open_list.append(child)
+
 
 if __name__ == '__main__':
     main()
